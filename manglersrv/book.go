392b633bf0b1188cf985a53307601ae9db6e0fae
Dominik Okwieka <dominik.okwieka@t-online.de>
2016-04-22 18:49:22 +0200

    renamed book struct to Book

--- manglersrv/book.go
+++ manglersrv/book.go
@@ -7,8 +7,8 @@ import (
 
 type ISBN string
 
-// struct book implements elem. XXX
-type book struct {
+// struct Book implements elem. XXX
+type Book struct {
 	isbn  ISBN
 	title string
 	notes []string
@@ -16,21 +16,21 @@ type book struct {
 }
 
 // books contains all book structs.
-var books map[ISBN]book
+var books map[ISBN]Book
 
-func (b book) String() string {
+func (b Book) String() string {
 	return string(b.isbn)
 }
 
-func (b book) Print() string {
+func (b Book) Print() string {
 	return fmt.Sprintln(b.isbn, b.title, b.notes) // XXX implement format as in spec
 }
 
-func (b book) Note(note string) {
+func (b Book) Note(note string) {
 	b.notes = append(b.notes, note)
 }
 
-func (b book) Delete() {
+func (b Book) Delete() {
 	// XXX delete from disk
 	// XXX should this return an error?
 	delete(books, b.isbn)
