45362930437e5bfdb5e61325375677f0a107e0b7
Dominik Okwieka <dominik.okwieka@t-online.de>
2016-04-22 16:52:57 +0200

    added isISBN13 function and test

--- manglersrv/book.go
+++ manglersrv/book.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
 
 type ISBN string
 
@@ -31,4 +34,51 @@ func (b book) Delete() {
 	// XXX delete from disk
 	// XXX should this return an error?
 	delete(books, b.isbn)
+}
+
+// The isISBN13 function checks whether its argument is a valid ISBN-13.
+// The input string may have dashes between the components. This is not required,
+// the check bases solely on length and checksum.
+// Examples: "978-0-201-07981-4", "9780201079814"
+func isISBN13(s string) bool {
+	if len(s) < 13 {
+		return false
+	}
+
+	var isbn [13]int
+	ndigits := 0
+	for i, r := range s {
+		// s has more runes than 13 digits + 4 dashes or more digits than expected.
+		if i > 17 || ndigits > 13 {
+			return false
+		}
+
+		if r == '-' {
+			continue
+		}
+
+		if unicode.IsDigit(r) {
+			isbn[ndigits] = int(r) - '0'
+			ndigits++
+		}
+	}
+
+	// Checksum calculation. Note that the odd digits have even indexes in the array.
+
+	sum := 0
+	for i := 0; i < 12; i += 2 {
+		sum += isbn[i] // odd numbers
+	}
+
+	for i := 1; i < 12; i += 2 {
+		sum += 3 * isbn[i] // even numbers
+	}
+
+	chksum := 10 - sum%10
+
+	if chksum != isbn[12] {
+		return false
+	}
+
+	return true
 }
---
+++ manglersrv/book_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+var isISBN13Tests = []struct {
+	s    string
+	pass bool
+}{
+	{"9780201079814", true},
+	{"978-0-201-07981-4", true},
+
+	{"", false},
+	{"abcdefghijklmn", false},
+	//{"978--0-201-07981-4", false}, // too many dashes; minor offence, ignored for now
+	{"978-0-201-07981-9", false}, // bad checksum
+	{"3945856", false},
+	{"0-201-07981-X", false}, // ISBN-10
+}
+
+func TestIsISBN13(t *testing.T) {
+	for _, tt := range isISBN13Tests {
+		p := isISBN13(tt.s)
+		if p != tt.pass {
+			t.Errorf("isISBN13(%q): got %v, want %v\n", tt.s, p, tt.pass)
+		}
+	}
+}
