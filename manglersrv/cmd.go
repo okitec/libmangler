6f47ba2ef2b7475a65a3e86c4427c136630a280a
Dominik Okwieka <dominik.okwieka@t-online.de>
2016-04-30 01:20:11 +0200

    added A and a commands

--- manglersrv/cmd.go
+++ manglersrv/cmd.go
@@ -15,12 +15,47 @@ type simpleCmdFn func(rw io.ReadWriter, args []string) error
 // "It is the error implementation's responsibility to summarize the context."
 // We don't do that here; the caller adds a "cmd %v" prefix for logging.
 
+// XXX get rid of stupid "s := "..."; fmt.Fprintln(rw, s); return errors.New(s)" dances
 var simpleCmdtab = map[rune]simpleCmdFn{
 	'A': func(rw io.ReadWriter, args []string) error {
-		return errors.New("unimplemented")
+		if len(args) < 2 {
+			s := "specify ISBN"
+			fmt.Fprintln(rw, s)
+			return errors.New(s)
+		}
+
+		_, err := NewBook(args[1], "foo", nil)  // XXX fetch or ask for title and author
+		return err
 	},
 	'a': func(rw io.ReadWriter, args []string) error {
-		return errors.New("unimplemented")
+		var b *Book
+		var n int
+		var err error
+		var ok bool
+
+		if len(args) < 3 {
+			s := "specify ISBN and number of books"
+			fmt.Fprintln(rw, s)
+			return errors.New(s)
+		}
+
+		if b, ok = books[ISBN(args[1])]; !ok {
+			s := "book doesn't exist"
+			fmt.Fprintln(rw, s)
+			return errors.New(s)
+		}
+
+		if n, err = strconv.Atoi(args[2]); err != nil {
+			s := "not a number"
+			fmt.Fprintln(rw, s)
+			return errors.New(s)
+		}
+
+		for i := 0; i < n; i++ {
+			NewCopy(b)
+		}
+
+		return nil
 	},
 	'u': func(rw io.ReadWriter, args []string) error {
 		return errors.New("unimplemented")
