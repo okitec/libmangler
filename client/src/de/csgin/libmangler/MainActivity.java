ffcea66197a312b0cb12265b5fbaf206b7dda416
Dominik Okwieka <dominik.okwieka@t-online.de>
2016-11-04 12:46:34 +0100

    Fix #45: remove settings menu

--- client/src/de/csgin/libmangler/MainActivity.java
+++ client/src/de/csgin/libmangler/MainActivity.java
@@ -113,18 +113,6 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	@Override
 	protected void onSaveInstanceState(Bundle out) {
 		super.onSaveInstanceState(out);
 		out.putLong("id", id);
